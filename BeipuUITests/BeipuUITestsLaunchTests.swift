8a4824bdfe4911d1904f054e356dfa8c9affdaab
Mike <>
2022-06-24 16:09:17 +0800

    project init

---
+++ BeipuUITests/BeipuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BeipuUITestsLaunchTests.swift
+//  BeipuUITests
+//
+//  Created by 陳Mike on 2022/6/24.
+//
+
+import XCTest
+
+class BeipuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
